21feaab333d8d0a6291971eb84d69ad6cb0cb1ee
Trevor <voxelmc@hotmail.com>
2023-10-16 23:56:45 -0700

    🚓 | Security Patch - 1.0.1
    - Remove script vulnerability from cache loading feature

--- src/atoms/citationsMap.ts
+++ src/atoms/citationsMap.ts
@@ -1,11 +1,25 @@
-
+import crypto from 'crypto';
 import { persistentAtom } from '@nanostores/persistent';
+import sanitizeHtml from 'sanitize-html';
+// const key = crypto.randomBytes(32);
+// const iv = crypto.randomBytes(16);
+
+// TODO: Make an api to get a key for this, and store it in a server side nanostore.
+// NOTE: Consider using server-storage for all of the citations for every user, as it is just text. 
+// However, using the cryptography algorithm allows me to use the localStorage, which is better for performance.
+
+// const key = crypto.scryptSync("12340918234720398474774234987", 'GfG', 24);
+// const iv = Buffer.alloc(16, 0);
+// const algorithm = 'aes-256-cbc';
 
 function getOuterHtml(elements: HTMLElement[]): string {
 	let result: string[] = [];
+	// let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
 	for (const element of elements) {
+		if (element == null) continue;
 		result.push(element.outerHTML);
 	}
+	// return cipher.update(JSON.stringify(result)).toString('hex');
 	return JSON.stringify(result);
 }
 
@@ -14,12 +28,15 @@ function parseCitations(value: string): HTMLElement[] {
 		console.log(value);
 		return [];
 	}
+	// let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+
+	// value = decipher.update(value, 'hex', 'utf8') + decipher.final('utf8');
 
 	let result: HTMLElement[] = [];
 	const elements = JSON.parse(value);
 	for (const element of elements) {
 		const div = document.createElement('div');
-		div.innerHTML = element;
+		div.innerHTML = sanitizeHtml(element);
 		result.push(div.firstChild as HTMLElement);
 	}
 	return result;
