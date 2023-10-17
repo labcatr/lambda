d0887ae5563c514791a18addfed16f58bcfa80c2
Trevor <voxelmc@hotmail.com>
2023-10-17 13:43:14 -0700

    ✏️ | Add React ⚛️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+// import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
+  // This is here if I ever want this to be server-side rendered
+  // output: "server",
+  // adapter: vercel()
 });
